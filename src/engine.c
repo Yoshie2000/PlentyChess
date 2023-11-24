2e786c78ffe54f98cbac4169ad1fb8f5b16f65b6
Yoshie2000 <patrick.leonhardt@gmx.net>
2023-11-24 18:10:53 +0100

    UCI Search Mock

--- src/engine.c
+++ src/engine.c
@@ -72,29 +72,29 @@ void* runEngine() {
         if (searchStopped)
             break;
 
-        printf("Pretending to search...\n");
-    }
-    printf("Engine thread stopping\n");
-
-    // struct Board board;
-    // struct BoardStack stack, newStack;
-    // board.stack = &stack;
-    // startpos(&board);
+	struct Board board;
+    struct BoardStack stack, newStack;
+    board.stack = &stack;
+    startpos(&board);
 
     // 2k2Q2/ppp5/4p3/b7/8/1K5p/P4r2/8 b - - 0 32
     // r3k1r1/pp3p1p/6q1/2p1nN2/1N2p1B1/4P3/PPP2PPP/R2Q1RK1 w q - 3 15
     // rn1qk1nr/ppp1ppbp/6p1/3p1b2/3P3P/4PN2/PPP2PP1/RNBQKB1R w KQkq - 1 5
 
     // parseFen(&board, "rn1qk1nr/ppp1ppbp/6p1/3p1b2/3P3P/4PN2/PPP2PP1/RNBQKB1R w KQkq - 1 5");
-    // debugBoard(&board);
+    debugBoard(&board);
     //generateLastSqTable();
 
-    // clock_t begin = clock();
-    // uint64_t nodes = perft(&board, 6, 6);
-    // clock_t end = clock();
-    // double time = (double)(end - begin) / CLOCKS_PER_SEC;
-    // uint64_t nps = nodes / time;
-    // printf("Perft: %" PRIu64 " nodes in %fs => %" PRIu64 " nps\n", nodes, time, nps);
+    clock_t begin = clock();
+    uint64_t nodes = perft(&board, 6, 6);
+    clock_t end = clock();
+    double time = (double)(end - begin) / CLOCKS_PER_SEC;
+    uint64_t nps = nodes / time;
+    printf("Perft: %" PRIu64 " nodes in %fs => %" PRIu64 " nps\n", nodes, time, nps);
+
+    }
+    printf("Engine thread stopping\n");
+
     return NULL;
 }
 
