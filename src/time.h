ccd7f34d9efa960671582da7a4ede62114f59c4b
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-01-16 18:48:31 +0100

    Stop early when depth is cleared, search longer if necessary. Bench: 8444756

--- src/time.h
+++ src/time.h
@@ -6,4 +6,5 @@
 
 int64_t getTime();
 bool timeOver(SearchParameters* parameters, SearchData* data);
+bool timeOverDepthCleared(SearchParameters* parameters, SearchData* data);
 void initTimeManagement(Board* rootBoard, SearchParameters* parameters, SearchData* data);
