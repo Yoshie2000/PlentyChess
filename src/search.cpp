cbbbc671c09d21f8d198e91077bd83d37b515a47
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-01-25 11:15:42 +0100

    doDeeper in lmr. Bench: 3150267

--- src/search.cpp
+++ src/search.cpp
@@ -561,6 +561,9 @@ movesLoop:
             reducedDepth = std::clamp(reducedDepth, 1, newDepth);
             value = -search<NON_PV_NODE>(board, stack + 1, thread, reducedDepth, -(alpha + 1), -alpha, true);
 
+            bool doDeeperSearch = value > (bestValue + 49 + 2 * newDepth);
+            newDepth += doDeeperSearch;
+
             if (value > alpha && reducedDepth < newDepth) {
                 value = -search<NON_PV_NODE>(board, stack + 1, thread, newDepth, -(alpha + 1), -alpha, !cutNode);
 
