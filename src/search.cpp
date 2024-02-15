cec4a675403a6659636b81e6c0e1fc04f527e4db
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-02-15 10:14:38 +0100

    Don't see prune pv nodes. Bench: 2556703

--- src/search.cpp
+++ src/search.cpp
@@ -531,7 +531,7 @@ movesLoop:
                 continue;
 
             // SEE Pruning
-            if (depth < seeDepth && !SEE(board, move, SEE_MARGIN[depth][!capture]))
+            if (!pvNode && depth < seeDepth && !SEE(board, move, SEE_MARGIN[depth][!capture]))
                 continue;
 
         }
