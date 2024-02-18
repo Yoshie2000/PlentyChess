90a823823d97d07562713b0ba7a6f15352f64511
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-02-18 12:03:08 +0100

    Introduce doShallowerSearch in lmr. Bench: 3697550

--- src/search.cpp
+++ src/search.cpp
@@ -621,9 +621,10 @@ movesLoop:
 
             reducedDepth = std::clamp(reducedDepth, 1, newDepth);
             value = -search<NON_PV_NODE>(board, stack + 1, thread, reducedDepth, -(alpha + 1), -alpha, true);
-
+            
+            bool doShallowerSearch = !rootNode && value < bestValue + newDepth;
             bool doDeeperSearch = value > (bestValue + lmrDeeperBase + lmrDeeperFactor * newDepth);
-            newDepth += doDeeperSearch;
+            newDepth += doDeeperSearch - doShallowerSearch;
 
             if (value > alpha && reducedDepth < newDepth) {
                 value = -search<NON_PV_NODE>(board, stack + 1, thread, newDepth, -(alpha + 1), -alpha, !cutNode);
