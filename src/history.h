36fa0d005f91e6c09e95a621b617619a808a22e6
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-02-26 20:58:37 +0100

    Make all histories 16 bit integers. Bench: 4050837

--- src/history.h
+++ src/history.h
@@ -8,31 +8,31 @@
 
 class History {
 
-    int quietHistory[2][64][64];
+    int16_t quietHistory[2][64][64];
     Move counterMoves[64][64];
-    int continuationHistory[2][PIECE_TYPES][64][PIECE_TYPES][64];
-    int captureHistory[2][PIECE_TYPES][64][PIECE_TYPES];
-    Eval correctionHistory[2][CORRECTION_HISTORY_SIZE];
+    int16_t continuationHistory[2][PIECE_TYPES][64][PIECE_TYPES][64];
+    int16_t captureHistory[2][PIECE_TYPES][64][PIECE_TYPES];
+    int16_t correctionHistory[2][CORRECTION_HISTORY_SIZE];
 
 public:
     void initHistory();
 
     Eval correctStaticEval(Eval eval, Board* board);
-    void updateCorrectionHistory(Board* board, int bonus);
+    void updateCorrectionHistory(Board* board, int16_t bonus);
 
     int getHistory(Board* board, SearchStack* searchStack, Move move, bool isCapture);
 
-    int getQuietHistory(Board* board, Move move);
-    void updateQuietHistory(Board* board, Move move, int bonus);
+    int16_t getQuietHistory(Board* board, Move move);
+    void updateQuietHistory(Board* board, Move move, int16_t bonus);
 
     int getContinuationHistory(Board* board, SearchStack* stack, Move move);
-    void updateContinuationHistory(Board* board, SearchStack* stack, Move move, int bonus);
+    void updateContinuationHistory(Board* board, SearchStack* stack, Move move, int16_t bonus);
 
-    int* getCaptureHistory(Board* board, Move move);
-    void updateSingleCaptureHistory(Board* board, Move move, int bonus);
-    void updateCaptureHistory(Board* board, Move move, int bonus, Move* captureMoves, int captureMoveCount);
+    int16_t* getCaptureHistory(Board* board, Move move);
+    void updateSingleCaptureHistory(Board* board, Move move, int16_t bonus);
+    void updateCaptureHistory(Board* board, Move move, int16_t bonus, Move* captureMoves, int captureMoveCount);
 
-    void updateQuietHistories(Board* board, SearchStack* stack, Move move, int bonus, Move* quietMoves, int quietMoveCount);
+    void updateQuietHistories(Board* board, SearchStack* stack, Move move, int16_t bonus, Move* quietMoves, int quietMoveCount);
 
     Move getCounterMove(Move move);
     void setCounterMove(Move move, Move counter);
