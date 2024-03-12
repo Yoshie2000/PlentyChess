822a9a2c29556dcb7effa042c5720a25a427e6e7
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-03-12 12:20:14 +0100

    Fix false mate scores from qsearch. Bench: 3635623

--- src/search.cpp
+++ src/search.cpp
@@ -294,7 +294,7 @@ movesLoopQsearch:
             continue;
         }
 
-        if (!SEE(board, move, -107))
+        if ((futilityValue > -EVAL_INFINITE || moveCount > 0) && !SEE(board, move, -107))
             break;
 
         if (!isLegal(board, move))
@@ -419,7 +419,8 @@ Eval search(Board* board, SearchStack* stack, Thread* thread, int depth, Eval al
     if (board->stack->checkers) {
         stack->staticEval = EVAL_NONE;
         goto movesLoop;
-    } else if (excluded) {
+    }
+    else if (excluded) {
         unadjustedEval = eval = stack->staticEval;
     }
     else if (ttHit) {
@@ -625,7 +626,7 @@ movesLoop:
 
             if (cutNode)
                 reducedDepth--;
-            
+
             if (capture)
                 reducedDepth += moveHistory / lmrHistoryFactorCapture;
             else
@@ -633,7 +634,7 @@ movesLoop:
 
             reducedDepth = std::clamp(reducedDepth, 1, newDepth);
             value = -search<NON_PV_NODE>(board, stack + 1, thread, reducedDepth, -(alpha + 1), -alpha, true);
-            
+
             bool doShallowerSearch = !rootNode && value < bestValue + newDepth;
             bool doDeeperSearch = value > (bestValue + lmrDeeperBase + lmrDeeperFactor * newDepth);
             newDepth += doDeeperSearch - doShallowerSearch;
@@ -673,7 +674,7 @@ movesLoop:
                     updatePv(stack, move);
 
                 if (bestValue >= beta) {
-                    
+
                     int bonus = std::min(historyBonusBase + historyBonusFactor * (depth + (eval <= alpha) + (value - 250 > beta)), historyBonusMax);
                     if (!capture) {
                         // Update quiet killers
