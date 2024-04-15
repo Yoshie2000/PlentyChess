591bfb6d03f456cd5b24bdf210c73a11c9702db7
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-04-15 09:24:19 +0200

    Use move history in SEE pruning. Bench: 3138242

--- src/search.cpp
+++ src/search.cpp
@@ -619,7 +619,7 @@ movesLoop:
                 continue;
 
             // SEE Pruning
-            if (!pvNode && depth < seeDepth && !SEE(board, move, SEE_MARGIN[depth][!capture]))
+            if (!pvNode && depth < seeDepth && !SEE(board, move, SEE_MARGIN[depth][!capture] - moveHistory / 64))
                 continue;
 
         }
