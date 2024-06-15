9b188a2bd7b5b42c4a46814192dc3287cd909aa3
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-06-15 13:36:18 +0200

    Reduce more for high ttValues that weren't extended. Bench: 2522473

--- src/search.cpp
+++ src/search.cpp
@@ -669,7 +669,7 @@ movesLoop:
                 return singularBeta;
             // We didn't prove singularity and an excluded search couldn't beat beta, but if the ttValue can we still reduce the depth
             else if (ttValue >= beta)
-                extension = -2;
+                extension = -3;
             // We didn't prove singularity and an excluded search couldn't beat beta, but we are expected to fail low, so reduce
             else if (cutNode)
                 extension = -2;
