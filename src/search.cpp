ff5903d488eb313b0c17f998e7c0f074f6f626cc
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-06-17 19:35:40 +0200

    Slight tempo adjustment. Bench: 2611296

--- src/search.cpp
+++ src/search.cpp
@@ -483,7 +483,8 @@ Eval Thread::search(Board* board, SearchStack* stack, int depth, Eval alpha, Eva
 
     // Adjust quiet history based on how much the previous move changed static eval
     if ((stack - 1)->movedPiece != Piece::NONE && !(stack - 1)->capture && !(stack - 1)->inCheck && stack->ply > 1) {
-        int bonus = std::clamp(-5 * int(stack->staticEval + (stack - 1)->staticEval), -100, 100);
+        int evalDiff = int(stack->staticEval + (stack - 1)->staticEval) - 20;
+        int bonus = std::clamp(-5 * evalDiff, -100, 100);
         history.updateQuietHistory((stack - 1)->move, flip(board->stm), board, board->stack->previous, bonus);
     }
 
