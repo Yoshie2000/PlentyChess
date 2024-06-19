c92914ca905cad21c2aac2e92e2e7fa47893a3ab
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-06-19 14:16:33 +0200

    Punish moves for not being the highest depth of a thread. Bench: 2693967

--- src/search.cpp
+++ src/search.cpp
@@ -1067,11 +1067,14 @@ Thread* Thread::chooseBestThread() {
         minValue++;
 
         for (auto& th : threadPool->threads) {
-            for (auto& rm : th.get()->rootMoves) {
+            for (auto& rm : th->rootMoves) {
                 if (rm.value == -EVAL_INFINITE)
                     break;
-                // Votes weighted by depth and difference to the minimum value
-                votes[rm.move] += (rm.value - minValue + 10) * rm.depth;
+
+                if (rm.depth == th->rootMoves[0].depth)
+                    votes[rm.move] += (rm.value - minValue + 10) * rm.depth;
+                else
+                    votes[rm.move] -= 10 * (th->rootMoves[0].depth - rm.depth);
             }
         }
 
