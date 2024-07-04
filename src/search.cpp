c49774c7742726a9f1efcb375a5323296f9347ff
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-07-05 00:21:15 +0200

    More cutnode reduction. Bench: 2167771

--- src/search.cpp
+++ src/search.cpp
@@ -744,7 +744,7 @@ movesLoop:
                 reducedDepth--;
 
             if (cutNode)
-                reducedDepth -= 2;
+                reducedDepth -= 3;
 
             if (capture)
                 reducedDepth += moveHistory / lmrHistoryFactorCapture;
