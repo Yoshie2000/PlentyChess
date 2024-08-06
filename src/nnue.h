744be179ed726b9e1c83db1e2911b8c2906a7019
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-08-06 23:09:36 +0200

    Bigger HL, more data. Bench: 2720387

--- src/nnue.h
+++ src/nnue.h
@@ -167,7 +167,7 @@ inline int vecHaddEpi32(Vec vec) {
 #endif
 
 constexpr int INPUT_WIDTH = 768;
-constexpr int HIDDEN_WIDTH = 512;
+constexpr int HIDDEN_WIDTH = 768;
 
 constexpr uint8_t KING_BUCKET_LAYOUT[] = {
     0, 0, 0, 0, 0, 0, 0, 0,
