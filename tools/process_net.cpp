e7a2f62a2aef6b3b5a9bc4865172f5af455a6bec
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-08-17 13:03:09 +0200

    Bench: 2528430

--- tools/process_net.cpp
+++ tools/process_net.cpp
@@ -4,7 +4,7 @@
 #include <string.h>
 
 constexpr int INPUT_WIDTH = 768;
-constexpr int HIDDEN_WIDTH = 512;
+constexpr int HIDDEN_WIDTH = 1024;
 
 constexpr bool KING_BUCKETS_FACTORIZED = false;
 constexpr int KING_BUCKETS = 1;
