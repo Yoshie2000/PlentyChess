7af689d8d35c3fa4b01dd52839e9c50726d52541
Patrick Leonhardt <40612283+Yoshie2000@users.noreply.github.com>
2024-09-23 20:30:21 +0200

    More LMR in pv nodes (#249)
    Bench: 2152679

--- src/search.cpp
+++ src/search.cpp
@@ -747,7 +747,7 @@ movesLoop:
 
         // Very basic LMR: Late moves are being searched with less depth
         // Check if the move can exceed alpha
-        if (moveCount > lmrMcBase + lmrMcPv * pvNode && depth >= lmrMinDepth && (!capture || !ttPv)) {
+        if (moveCount > lmrMcBase + lmrMcPv * rootNode && depth >= lmrMinDepth && (!capture || !ttPv)) {
             int reducedDepth = newDepth - REDUCTIONS[!capture][depth][moveCount];
 
             if (!ttPv)
