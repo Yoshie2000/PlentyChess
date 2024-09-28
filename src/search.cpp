f0ff767876da1643168f5dfa9bdbf1f6758f0e67
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-09-28 21:05:28 +0200

    Also do LMR if cutnode. Bench: 2167568

--- src/search.cpp
+++ src/search.cpp
@@ -753,7 +753,7 @@ movesLoop:
 
         // Very basic LMR: Late moves are being searched with less depth
         // Check if the move can exceed alpha
-        if (moveCount > lmrMcBase + lmrMcPv * rootNode && depth >= lmrMinDepth && (!capture || !ttPv)) {
+        if (moveCount > lmrMcBase + lmrMcPv * rootNode && depth >= lmrMinDepth && (!capture || !ttPv || cutNode)) {
             int reducedDepth = newDepth - REDUCTIONS[!capture][depth][moveCount];
 
             if (!ttPv)
