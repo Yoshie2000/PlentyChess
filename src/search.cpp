2ab95aa20efccba13a3d8d8946cdd58ffe5d8574
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-10-04 10:08:52 +0200

    Bench: 2070114

--- src/search.cpp
+++ src/search.cpp
@@ -676,7 +676,7 @@ movesLoop:
                 continue;
 
             // SEE Pruning
-            if (!SEE(board, move, (2 + pvNode) * SEE_MARGIN[!capture ? lmrDepth : depth][!capture] / 2))
+            if (!SEE(board, move, (2 + 2 * pvNode) * SEE_MARGIN[!capture ? lmrDepth : depth][!capture] / 2))
                 continue;
 
         }
