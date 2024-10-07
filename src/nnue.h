75587c03ec4a9847b2e825a12c1312ac26a6fa98
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-10-07 19:18:23 +0200

    SIMD for all architectures. Bench: 1792885

--- src/nnue.h
+++ src/nnue.h
@@ -9,169 +9,277 @@
 
 #if defined(__AVX512F__) && defined(__AVX512BW__)
 
-using Vec = __m512i;
+using VecI = __m512i;
+using VecF = __m512;
 
-inline Vec addEpi16(Vec x, Vec y) {
+inline VecI addEpi16(VecI x, VecI y) {
   return _mm512_add_epi16(x, y);
 }
 
-inline Vec addEpi32(Vec x, Vec y) {
-  return _mm512_add_epi32(x, y);
-}
-
-inline Vec subEpi16(Vec x, Vec y) {
+inline VecI subEpi16(VecI x, VecI y) {
   return _mm512_sub_epi16(x, y);
 }
 
-inline Vec minEpi16(Vec x, Vec y) {
+inline VecI minEpi16(VecI x, VecI y) {
   return _mm512_min_epi16(x, y);
 }
 
-inline Vec maxEpi16(Vec x, Vec y) {
+inline VecI maxEpi16(VecI x, VecI y) {
   return _mm512_max_epi16(x, y);
 }
 
-inline Vec mulloEpi16(Vec x, Vec y) {
-  return _mm512_mullo_epi16(x, y);
+inline VecI set1Epi16(int i) {
+  return _mm512_set1_epi16(i);
 }
 
-inline Vec maddEpi16(Vec x, Vec y) {
-  return _mm512_madd_epi16(x, y);
+inline VecI set1Epi32(int i) {
+  return _mm512_set1_epi32(i);
 }
 
-inline Vec vecSetZero() {
-  return _mm512_setzero_si512();
+inline VecI slliEpi16(VecI x, int shift) {
+  return _mm512_slli_epi16(x, shift);
 }
 
-inline Vec vecSet1Epi16(int16_t x) {
-  return _mm512_set1_epi16(x);
+inline VecI mulhiEpi16(VecI x, VecI y) {
+  return _mm512_mulhi_epi16(x, y);
 }
 
-inline int vecHaddEpi32(Vec vec) {
-  return _mm512_reduce_add_epi32(vec);
+#if defined(__AVX512VNNI__)
+inline VecI dpbusdEpi32(VecI sum, VecI u, VecI i) {
+  return _mm512_dpbusd_epi32(sum, u, i);
+}
+#else
+inline VecI dpbusdEpi32(VecI sum, VecI u, VecI i) {
+  VecI sum32 = _mm512_madd_epi16(_mm512_maddubs_epi16(u, i), _mm512_set1_epi16(1));
+  return _mm512_add_epi32(sum32, sum);
+}
+#endif
+
+inline VecI packusEpi16(VecI x, VecI y) {
+  VecI packed = _mm512_packus_epi16(x, y);
+  packed = _mm512_permutexvar_epi64(_mm512_setr_epi64(0, 2, 4, 6, 1, 3, 5, 7), packed);
+  return packed;
+}
+
+inline void vecStoreI(VecI* dest, VecI x) {
+  _mm512_store_si512(dest, x);
+}
+
+inline VecF cvtepi32Ps(VecI x) {
+  return _mm512_cvtepi32_ps(x);
+}
+
+inline VecF addPs(VecF x, VecF y) {
+  return _mm512_add_ps(x, y);
+}
+
+inline VecF mulPs(VecF x, VecF y) {
+  return _mm512_mul_ps(x, y);
+}
+
+inline VecF set1Ps(float x) {
+  return _mm512_set1_ps(x);
+}
+
+inline VecF minPs(VecF x, VecF y) {
+  return _mm512_min_ps(x, y);
+}
+
+inline VecF maxPs(VecF x, VecF y) {
+  return _mm512_max_ps(x, y);
+}
+
+inline VecF fmaddPs(VecF a, VecF b, VecF c) {
+  return _mm512_fmadd_ps(a, b, c);
+}
+
+inline float reduceAddPs(VecF* v) {
+  return _mm512_reduce_add_ps(v[0]);
 }
 
 #elif defined(__AVX2__)
 
-using Vec = __m256i;
+using VecI = __m256i;
+using VecF = __m256;
 
-inline Vec addEpi16(Vec x, Vec y) {
+inline VecI addEpi16(VecI x, VecI y) {
   return _mm256_add_epi16(x, y);
 }
 
-inline Vec addEpi32(Vec x, Vec y) {
-  return _mm256_add_epi32(x, y);
-}
-
-inline Vec subEpi16(Vec x, Vec y) {
+inline VecI subEpi16(VecI x, VecI y) {
   return _mm256_sub_epi16(x, y);
 }
 
-inline Vec minEpi16(Vec x, Vec y) {
+inline VecI minEpi16(VecI x, VecI y) {
   return _mm256_min_epi16(x, y);
 }
 
-inline Vec maxEpi16(Vec x, Vec y) {
+inline VecI maxEpi16(VecI x, VecI y) {
   return _mm256_max_epi16(x, y);
 }
 
-inline Vec mulloEpi16(Vec x, Vec y) {
-  return _mm256_mullo_epi16(x, y);
+inline VecI set1Epi16(int i) {
+  return _mm256_set1_epi16(i);
 }
 
-inline Vec maddEpi16(Vec x, Vec y) {
-  return _mm256_madd_epi16(x, y);
+inline VecI set1Epi32(int i) {
+  return _mm256_set1_epi32(i);
 }
 
-inline Vec vecSetZero() {
-  return _mm256_setzero_si256();
+inline VecI slliEpi16(VecI x, int shift) {
+  return _mm256_slli_epi16(x, shift);
 }
 
-inline Vec vecSet1Epi16(int16_t x) {
-  return _mm256_set1_epi16(x);
+inline VecI mulhiEpi16(VecI x, VecI y) {
+  return _mm256_mulhi_epi16(x, y);
 }
 
-inline int vecHaddEpi32(Vec vec) {
-  __m128i xmm0;
-  __m128i xmm1;
+inline VecI packusEpi16(VecI x, VecI y) {
+  VecI packed = _mm256_packus_epi16(x, y);
+  packed = _mm256_permute4x64_epi64(packed, _MM_SHUFFLE(3, 1, 2, 0));
+  return packed;
+}
 
-  // Get the lower and upper half of the register:
-  xmm0 = _mm256_castsi256_si128(vec);
-  xmm1 = _mm256_extracti128_si256(vec, 1);
+inline void vecStoreI(VecI* dest, VecI x) {
+  _mm256_store_si256(dest, x);
+}
 
-  // Add the lower and upper half vertically:
-  xmm0 = _mm_add_epi32(xmm0, xmm1);
+inline VecI dpbusdEpi32(VecI sum, VecI u, VecI i) {
+  VecI sum32 = _mm256_madd_epi16(_mm256_maddubs_epi16(u, i), _mm256_set1_epi16(1));
+  return _mm256_add_epi32(sum32, sum);
+}
 
-  // Get the upper half of the result:
-  xmm1 = _mm_unpackhi_epi64(xmm0, xmm0);
+inline VecF cvtepi32Ps(VecI x) {
+  return _mm256_cvtepi32_ps(x);
+}
 
-  // Add the lower and upper half vertically:
-  xmm0 = _mm_add_epi32(xmm0, xmm1);
+inline VecF addPs(VecF x, VecF y) {
+  return _mm256_add_ps(x, y);
+}
 
-  // Shuffle the result so that the lower 32-bits are directly above the second-lower 32-bits:
-  xmm1 = _mm_shuffle_epi32(xmm0, _MM_SHUFFLE(2, 3, 0, 1));
+inline VecF mulPs(VecF x, VecF y) {
+  return _mm256_mul_ps(x, y);
+}
 
-  // Add the lower 32-bits to the second-lower 32-bits vertically:
-  xmm0 = _mm_add_epi32(xmm0, xmm1);
+inline VecF set1Ps(float x) {
+  return _mm256_set1_ps(x);
+}
 
-  // Cast the result to the 32-bit integer type and return it:
-  return _mm_cvtsi128_si32(xmm0);
+inline VecF minPs(VecF x, VecF y) {
+  return _mm256_min_ps(x, y);
+}
+
+inline VecF maxPs(VecF x, VecF y) {
+  return _mm256_max_ps(x, y);
+}
+
+inline VecF fmaddPs(VecF a, VecF b, VecF c) {
+  return _mm256_fmadd_ps(a, b, c);
+}
+
+inline float reduceAddPs(VecF* v) {
+  v[0] = _mm256_add_ps(v[0], v[1]);
+  __m128 high = _mm256_extractf128_ps(v[0], 1);
+  __m128 low = _mm256_castps256_ps128(v[0]);
+  __m128 sum = _mm_add_ps(high, low);
+  __m128 high64 = _mm_movehl_ps(sum, sum);
+  __m128 sum64 = _mm_add_ps(sum, high64);
+
+  return ((float*)&sum64)[0] + ((float*)&sum64)[1];
 }
 
 #else
 
-using Vec = __m128i;
+using VecI = __m128i;
+using VecF = __m128;
 
-inline Vec addEpi16(Vec x, Vec y) {
+inline VecI addEpi16(VecI x, VecI y) {
   return _mm_add_epi16(x, y);
 }
 
-inline Vec addEpi32(Vec x, Vec y) {
-  return _mm_add_epi32(x, y);
-}
-
-inline Vec subEpi16(Vec x, Vec y) {
+inline VecI subEpi16(VecI x, VecI y) {
   return _mm_sub_epi16(x, y);
 }
 
-inline Vec minEpi16(Vec x, Vec y) {
+inline VecI minEpi16(VecI x, VecI y) {
   return _mm_min_epi16(x, y);
 }
 
-inline Vec maxEpi16(Vec x, Vec y) {
+inline VecI maxEpi16(VecI x, VecI y) {
   return _mm_max_epi16(x, y);
 }
 
-inline Vec mulloEpi16(Vec x, Vec y) {
-  return _mm_mullo_epi16(x, y);
+inline VecI set1Epi16(int i) {
+  return _mm_set1_epi16(i);
 }
 
-inline Vec maddEpi16(Vec x, Vec y) {
-  return _mm_madd_epi16(x, y);
+inline VecI set1Epi32(int i) {
+  return _mm_set1_epi32(i);
 }
 
-inline Vec vecSetZero() {
-  return _mm_setzero_si128();
+inline VecI slliEpi16(VecI x, int shift) {
+  return _mm_slli_epi16(x, shift);
 }
 
-inline Vec vecSet1Epi16(int16_t x) {
-  return _mm_set1_epi16(x);
+inline VecI mulhiEpi16(VecI x, VecI y) {
+  return _mm_mulhi_epi16(x, y);
 }
 
-inline int vecHaddEpi32(Vec vec) {
-  int* asArray = (int*)&vec;
-  return asArray[0] + asArray[1] + asArray[2] + asArray[3];
+inline VecI packusEpi16(VecI x, VecI y) {
+  return _mm_packus_epi16(x, y);
 }
 
-inline float vecReduceAddPs(float* sums, int length) {
-    // for (int i = 0; i < length; i++)
-    //   std::cout << std::to_string(((int*)sums)[i]) << " ";
-    // std::cout << std::endl << std::endl;
-    if (length == 2) return sums[0] + sums[1];
-    for (int i = 0; i < length / 2; ++i)
-        sums[i] += sums[i + length / 2];
+inline void vecStoreI(VecI* dest, VecI x) {
+  _mm_store_si128(dest, x);
+}
 
-    return vecReduceAddPs(sums, length / 2);
+#if defined(__SSSE3__)
+inline VecI dpbusdEpi32(VecI sum, VecI u, VecI i) {
+  VecI sum32 = _mm_madd_epi16(_mm_maddubs_epi16(u, i), _mm_set1_epi16(1));
+  return _mm_add_epi32(sum32, sum);
+}
+#endif
+
+inline VecF cvtepi32Ps(VecI x) {
+  return _mm_cvtepi32_ps(x);
+}
+
+inline VecF addPs(VecF x, VecF y) {
+  return _mm_add_ps(x, y);
+}
+
+inline VecF mulPs(VecF x, VecF y) {
+  return _mm_mul_ps(x, y);
+}
+
+inline VecF set1Ps(float x) {
+  return _mm_set1_ps(x);
+}
+
+inline VecF minPs(VecF x, VecF y) {
+  return _mm_min_ps(x, y);
+}
+
+inline VecF maxPs(VecF x, VecF y) {
+  return _mm_max_ps(x, y);
+}
+
+#if defined(__FMA__)
+inline VecF fmaddPs(VecF a, VecF b, VecF c) {
+  return _mm_fmadd_ps(a, b, c);
+}
+#endif
+
+inline float reduceAddPsR(float* sums, int length) {
+  if (length == 2) return sums[0] + sums[1];
+  length /= 2;
+  for (int i = 0; i < length; ++i)
+    sums[i] += sums[i + length];
+  return reduceAddPsR(sums, length);
+}
+
+inline float reduceAddPs(VecF* sums) {
+  return reduceAddPsR((float*) sums, 64 / sizeof(float));
 }
 
 #endif
@@ -203,10 +311,11 @@ constexpr float L1_NORMALISATION = static_cast<float>(1 << INPUT_SHIFT) / static
 
 constexpr int ALIGNMENT = 64;
 
-constexpr int I16_VEC_SIZE = sizeof(Vec) / sizeof(int16_t);
-constexpr int FLOAT_VEC_SIZE = sizeof(__m256) / sizeof(float);
+constexpr int I16_VEC_SIZE = sizeof(VecI) / sizeof(int16_t);
+constexpr int I32_VEC_SIZE = sizeof(VecI) / sizeof(int32_t);
+constexpr int FLOAT_VEC_SIZE = sizeof(VecF) / sizeof(float);
 
-constexpr int INT8_PER_INT32 = sizeof(int) / sizeof(int8_t);
+constexpr int INT8_PER_INT32 = sizeof(int32_t) / sizeof(int8_t);
 
 constexpr int L1_ITERATIONS = L1_SIZE / I16_VEC_SIZE;
 
