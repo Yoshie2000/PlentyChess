68b447fb3c987bb8ad5afba62ab1477e71fe86b0
Patrick Leonhardt <40612283+Yoshie2000@users.noreply.github.com>
2024-10-16 22:04:57 +0200

    Increase minimum and maximum NMP reduction (#263)
    Bench: 1810929

--- src/search.cpp
+++ src/search.cpp
@@ -73,10 +73,10 @@ TUNE_INT(rfpFactor, 75, 1, 250);
 TUNE_INT_DISABLED(razoringDepth, 5, 2, 20);
 TUNE_INT(razoringFactor, 204, 1, 1000);
 
-TUNE_INT_DISABLED(nmpRedBase, 3, 1, 5);
+TUNE_INT_DISABLED(nmpRedBase, 4, 1, 5);
 TUNE_INT_DISABLED(nmpDepthDiv, 3, 1, 6);
-TUNE_INT_DISABLED(nmpMin, 3, 1, 10);
-TUNE_INT(nmpDivisor, 24, 10, 1000);
+TUNE_INT_DISABLED(nmpMin, 4, 1, 10);
+TUNE_INT(nmpDivisor, 60, 10, 1000);
 
 TUNE_INT(probCutBetaOffset, 191, 1, 500);
 TUNE_INT_DISABLED(probCutDepth, 5, 1, 15);
