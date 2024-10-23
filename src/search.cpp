edbbc0d52939bd2662934ed3ec2227816c75f78a
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-10-23 11:51:53 +0200

    Bench: 1479022

--- src/search.cpp
+++ src/search.cpp
@@ -875,13 +875,14 @@ movesLoop:
     bool failLow = alpha == oldAlpha;
     bool failHigh = bestValue >= beta;
     int flags = failHigh ? TT_LOWERBOUND : !failLow ? TT_EXACTBOUND : TT_UPPERBOUND;
-    if (!excluded)
+    if (!excluded) {
         ttEntry->update(board->stack->hash, bestMove, depth, unadjustedEval, valueToTT(bestValue, stack->ply), ttPv, flags);
 
-    // Adjust correction history
-    if (!board->stack->checkers && (bestMove == MOVE_NONE || !board->isCapture(bestMove)) && (!failHigh || bestValue > stack->staticEval) && (!failLow || bestValue <= stack->staticEval)) {
-        int bonus = std::clamp((int)(bestValue - stack->staticEval) * depth * correctionHistoryFactor / 1024, -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
-        history.updateCorrectionHistory(board, stack, bonus);
+        // Adjust correction history
+        if (!board->stack->checkers && (bestMove == MOVE_NONE || !board->isCapture(bestMove)) && (!failHigh || bestValue > stack->staticEval) && (!failLow || bestValue <= stack->staticEval)) {
+            int bonus = std::clamp((int)(bestValue - stack->staticEval) * depth * correctionHistoryFactor / 1024, -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
+            history.updateCorrectionHistory(board, stack, bonus);
+        }
     }
 
     assert(bestValue > -EVAL_INFINITE && bestValue < EVAL_INFINITE);
