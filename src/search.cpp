51572f3abaa679b9ccaa84184910a5b17bcac2b3
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-10-23 13:27:38 +0200

    Bench: 1745215

--- src/search.cpp
+++ src/search.cpp
@@ -42,7 +42,7 @@ TUNE_FLOAT(aspirationWindowDeltaFactor, 1.5804938062670641f, 1.0f, 3.0f);
 // Reduction / Margin tables
 TUNE_FLOAT(lmrReductionNoisyBase, -0.5888240580751226f, -2.0f, -0.1f);
 TUNE_FLOAT(lmrReductionNoisyFactor, 3.1133946268983235f, 2.0f, 4.0f);
-TUNE_FLOAT(lmrReductionQuietBase, 0.4818295641170254f, 0.50f, 1.5f);
+TUNE_FLOAT(lmrReductionQuietBase, 0.7818295641170254f, 0.50f, 1.5f);
 TUNE_FLOAT(lmrReductionQuietFactor, 2.9415810588232394f, 2.0f, 4.0f);
 
 TUNE_FLOAT(seeMarginNoisy, -22.026705241168507f, -50.0f, -10.0f);
