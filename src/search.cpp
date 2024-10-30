cf05bb2c4bbdcc54b2d2c6082f599260c43c61cc
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-10-30 19:42:11 +0100

    Bench: 1553280

--- src/search.cpp
+++ src/search.cpp
@@ -435,7 +435,7 @@ Eval Thread::search(Board* board, SearchStack* stack, int depth, Eval alpha, Eva
     Move excludedMove = stack->excludedMove;
     Eval bestValue = -EVAL_INFINITE;
     Eval oldAlpha = alpha;
-    bool improving = false, worsening = false, skipQuiets = false, excluded = excludedMove != MOVE_NONE;
+    bool improving = true, worsening = false, skipQuiets = false, excluded = excludedMove != MOVE_NONE;
 
     (stack + 1)->killer = MOVE_NONE;
     (stack + 1)->excludedMove = MOVE_NONE;
