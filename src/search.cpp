176832e399589a8980a02d4ea5600900068b6354
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-11-01 16:58:47 +0100

    Bench: 1726986

--- src/search.cpp
+++ src/search.cpp
@@ -914,7 +914,8 @@ movesLoop:
 
     // Adjust correction history
     if (!board->stack->checkers && (bestMove == MOVE_NONE || !board->isCapture(bestMove)) && (!failHigh || bestValue > stack->staticEval) && (!failLow || bestValue <= stack->staticEval)) {
-        int bonus = std::clamp((int)(bestValue - stack->staticEval) * depth * correctionHistoryFactor / 1024, -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
+        int factor = depth + (2 * pvNode);
+        int bonus = std::clamp((int)(bestValue - stack->staticEval) * factor * correctionHistoryFactor / 1024, -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
         history.updateCorrectionHistory(board, stack, bonus);
     }
 
