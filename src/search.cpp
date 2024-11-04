023eb0d713813c0174dcfef888ae962448340a19
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-11-04 09:45:25 +0100

    Bench: 1609436

--- src/search.cpp
+++ src/search.cpp
@@ -491,7 +491,7 @@ Eval Thread::search(Board* board, SearchStack* stack, int depth, Eval alpha, Eva
 
     // IIR
     if ((!ttHit || ttDepth + 4 < depth) && depth >= iirMinDepth)
-        depth -= 1 + (pvNode && !ttHit);
+        depth -= 1 + (pvNode && !ttHit && depth >= 9);
 
     // Improving
     if ((stack - 2)->staticEval != EVAL_NONE) {
