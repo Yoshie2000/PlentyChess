e3ea5fc9fc3db80da3dddf81a45e84f835492a91
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-11-07 23:04:13 +0100

    Bench: 1915423

--- src/search.cpp
+++ src/search.cpp
@@ -438,6 +438,7 @@ Eval Thread::search(Board* board, SearchStack* stack, int depth, Eval alpha, Eva
     Eval bestValue = -EVAL_INFINITE;
     Eval oldAlpha = alpha;
     bool improving = false, worsening = false, skipQuiets = false, excluded = excludedMove != MOVE_NONE;
+    bool oppEasyCapture = board->opponentHasGoodCapture();
 
     (stack + 1)->killer = MOVE_NONE;
     (stack + 1)->excludedMove = MOVE_NONE;
@@ -512,7 +513,7 @@ Eval Thread::search(Board* board, SearchStack* stack, int depth, Eval alpha, Eva
     }
 
     // Reverse futility pruning
-    if (!rootNode && depth < rfpDepth && std::abs(eval) < EVAL_MATE_IN_MAX_PLY && eval - rfpFactor * (depth - (improving && !board->opponentHasGoodCapture())) >= beta)
+    if (!rootNode && depth < rfpDepth && std::abs(eval) < EVAL_MATE_IN_MAX_PLY && eval - rfpFactor * (depth - (improving && !oppEasyCapture)) >= beta)
         return std::min((eval + beta) / 2, EVAL_MATE_IN_MAX_PLY - 1);
 
     // Razoring
@@ -780,7 +781,7 @@ movesLoop:
             else
                 reducedDepth += moveHistory / lmrHistoryFactorQuiet;
 
-            if (worsening)
+            if (!improving && oppEasyCapture)
                 reducedDepth--;
 
             reducedDepth = std::clamp(reducedDepth, 1, newDepth);
