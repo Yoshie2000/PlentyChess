399740390683d1eb798c0c27d7e1806c680dd3e5
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-11-18 16:15:11 +0100

    Bench: 1809269

--- src/search.cpp
+++ src/search.cpp
@@ -67,7 +67,7 @@ TUNE_INT(staticHistoryFactor, -36, -500, -1);
 TUNE_INT(staticHistoryMin, -79, -1000, -1);
 TUNE_INT(staticHistoryMax, 123, 1, 1000);
 
-TUNE_INT_DISABLED(rfpDepth, 8, 2, 20);
+TUNE_INT_DISABLED(rfpDepth, 12, 2, 20);
 TUNE_INT(rfpFactor, 77, 1, 250);
 
 TUNE_INT_DISABLED(razoringDepth, 5, 2, 20);
