9d3acae7f6d83be18d8f083f3e1438aaacd00aaf
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-11-21 09:34:01 +0100

    Increment version + calibrate WDL model. Bench: 1886365

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -58,7 +58,7 @@ std::string formatEval(Eval value) {
         evalString = "mate " + std::to_string(-(EVAL_MATE + value) / 2);
     }
     else {
-        evalString = "cp " + std::to_string(100 * value / 261);
+        evalString = "cp " + std::to_string(100 * value / 220);
     }
     return evalString;
 }
