b93fb0e44ab273a6e801c024c8ea7b2ee860a4d0
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-02-09 19:09:46 +0100

    Bench: 2016287

--- src/search.cpp
+++ src/search.cpp
@@ -65,8 +65,8 @@ TUNE_INT_DISABLED(qsSeeMargin, -101, -200, 50);
 TUNE_INT_DISABLED(iirMinDepth, 4, 1, 10);
 
 TUNE_INT(staticHistoryFactor, -49, -500, -1);
-TUNE_INT(staticHistoryMin, -82, -1000, -1);
-TUNE_INT(staticHistoryMax, 144, 1, 1000);
+TUNE_INT(staticHistoryMin, -250, -1000, -1);
+TUNE_INT(staticHistoryMax, 400, 1, 1000);
 
 TUNE_INT_DISABLED(rfpDepth, 8, 2, 20);
 TUNE_INT(rfpFactor, 86, 1, 250);
@@ -570,7 +570,7 @@ Eval Thread::search(Board* board, SearchStack* stack, int depth, Eval alpha, Eva
 
     // Adjust quiet history based on how much the previous move changed static eval
     if (!excluded && (stack - 1)->movedPiece != Piece::NONE && !(stack - 1)->capture && !(stack - 1)->inCheck && stack->ply > 1) {
-        int bonus = std::clamp(staticHistoryFactor * int(stack->staticEval + (stack - 1)->staticEval) / 10, staticHistoryMin, staticHistoryMax);
+        int bonus = std::clamp(staticHistoryFactor * int(stack->staticEval + (stack - 1)->staticEval + 50) / 10, staticHistoryMin, staticHistoryMax);
         history.updateQuietHistory((stack - 1)->move, flip(board->stm), board, board->stack->previous, bonus);
     }
 
