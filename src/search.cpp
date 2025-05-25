1b1c029c95edd78da86a420053a0b97cf9aa20f6
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-05-25 17:20:36 +0200

    Bench: 2124421

--- src/search.cpp
+++ src/search.cpp
@@ -734,7 +734,7 @@ movesLoop:
             ) {
 
             int lmrDepth = std::max(0, depth - earlyLmrReductionTableFactor * REDUCTIONS[!capture][depth][moveCount] / 1000000 - !improving + moveHistory / (capture ? earlyLmrHistoryFactorCapture : earlyLmrHistoryFactorQuiet));
-            lmrDepth = std::min(MAX_PLY - 1, lmrDepth);
+            lmrDepth = std::min(depth + 1, lmrDepth);
 
             if (!pvNode && !movegen.skipQuiets) {
 
