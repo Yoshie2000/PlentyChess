7280de6998dc1af9ad46f3f9292df0e6e3ddd999
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-05-30 09:13:52 +0200

    Bench: 1964783

--- src/search.cpp
+++ src/search.cpp
@@ -755,11 +755,11 @@ movesLoop:
                     continue;
             }
 
-            lmrDepth = std::min(std::min(depth + 1, MAX_PLY - 1), lmrDepth);
+            lmrDepth = std::min(MAX_PLY - 1, lmrDepth);
 
             // History pruning
             int hpFactor = capture ? historyPruningFactorCapture : historyPruningFactorQuiet;
-            if (!pvNode && lmrDepth < historyPruningDepth && moveHistory < hpFactor * depth)
+            if (!pvNode && std::min(depth + 1, lmrDepth) < historyPruningDepth && moveHistory < hpFactor * depth)
                 continue;
 
             // SEE Pruning
