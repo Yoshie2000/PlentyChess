4a7a0b9c98cd468648f3dba685d2ad97b0658833
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-06-29 13:14:29 +0200

    Bench: 2466902

--- src/search.cpp
+++ src/search.cpp
@@ -236,22 +236,22 @@ int valueFromTt(int value, int ply, int rule50) {
         // Downgrade potentially false mate score
         if (value >= EVAL_MATE_IN_MAX_PLY && EVAL_MATE - value > 100 - rule50)
             return EVAL_TBWIN_IN_MAX_PLY - 1;
-        
+
         // Downgrade potentially false TB score
         if (EVAL_TBWIN - value > 100 - rule50)
             return EVAL_TBWIN_IN_MAX_PLY - 1;
-        
+
         return value - ply;
     }
     else if (value <= -EVAL_TBWIN_IN_MAX_PLY) {
         // Downgrade potentially false mate score
         if (value <= -EVAL_MATE_IN_MAX_PLY && EVAL_MATE + value > 100 - rule50)
             return -EVAL_TBWIN_IN_MAX_PLY + 1;
-        
+
         // Downgrade potentially false TB score
         if (EVAL_TBWIN + value > 100 - rule50)
             return -EVAL_TBWIN_IN_MAX_PLY + 1;
-        
+
         return value + ply;
     }
     return value;
@@ -993,7 +993,8 @@ movesLoop:
             if (stack->ttPv && !pvNode && !cutNode && capture) {
                 // Do very slight LMR for captures in ttPv-allnodes
                 reduction /= 2;
-            } else {
+            }
+            else {
                 if (boardCopy->checkers)
                     reduction -= lmrCheck;
 
@@ -1007,12 +1008,12 @@ movesLoop:
                     reduction += lmrTtpvFaillow;
 
                 reduction -= std::abs(correctionValue / lmrCorrection);
-
-                if (capture)
-                    reduction -= moveHistory * std::abs(moveHistory) / lmrHistoryFactorCapture;
-                else
-                    reduction -= 1000 * moveHistory / lmrHistoryFactorQuiet;
             }
+
+            if (capture)
+                reduction -= moveHistory * std::abs(moveHistory) / lmrHistoryFactorCapture;
+            else
+                reduction -= 1000 * moveHistory / lmrHistoryFactorQuiet;
 
             int reducedDepth = std::clamp(newDepth - reduction / 1000, 1, newDepth + pvNode);
             stack->reduction = reduction;
