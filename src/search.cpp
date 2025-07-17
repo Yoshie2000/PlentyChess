77542deddd4b000fddfe07235a3ee15934061e53
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-07-17 12:31:34 +0200

    Bench: 2207779

--- src/search.cpp
+++ src/search.cpp
@@ -886,7 +886,7 @@ movesLoop:
             // Futility pruning for captures
             if (!pvNode && capture && moveType(move) != MOVE_PROMOTION) {
                 Piece capturedPiece = moveType(move) == MOVE_ENPASSANT ? Piece::PAWN : board->pieces[moveTarget(move)];
-                if (lmrDepth < fpCaptDepth && eval + fpCaptBase + PIECE_VALUES[capturedPiece] + fpCaptFactor * lmrDepth <= alpha)
+                if (lmrDepth < fpCaptDepth && eval + fpCaptBase + PIECE_VALUES[capturedPiece] + fpCaptFactor * lmrDepth + moveHistory / 150 <= alpha)
                     continue;
             }
 
