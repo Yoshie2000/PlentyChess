dcfd1b74ad9e531f80c730cac09f78cc1fbdafc8
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-07-22 17:25:27 +0200

    Bench: 1653310

--- src/tt.cpp
+++ src/tt.cpp
@@ -7,7 +7,7 @@ void TTEntry::update(uint64_t _hash, Move _bestMove, int16_t _depth, Eval _eval,
     if (_bestMove != MOVE_NONE || (uint16_t)_hash != hash)
         bestMove = _bestMove;
 
-    if (_flags == TT_EXACTBOUND || (uint16_t)_hash != hash || _depth + 200 * wasPv + 400 > depth) {
+    if (_flags == TT_EXACTBOUND || (uint16_t)_hash != hash || _depth + 200 * wasPv + 500 >= depth) {
         hash = (uint16_t)_hash;
         depth = _depth;
         value = _value;
