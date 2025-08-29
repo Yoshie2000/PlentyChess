edeaec9135945f5a0bba1798aa87a6619e11fa13
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-08-29 10:15:29 +0200

    Bench: 1779226

--- src/search.cpp
+++ src/search.cpp
@@ -1006,10 +1006,12 @@ movesLoop:
 
         // Very basic LMR: Late moves are being searched with less depth
         // Check if the move can exceed alpha
-        if (moveCount > lmrMcBase + lmrMcPv * rootNode - (ttMove != MOVE_NONE) && depth >= lmrMinDepth && (!capture || !pvNode)) {
+        if (moveCount > lmrMcBase + lmrMcPv * rootNode - (ttMove != MOVE_NONE) && depth >= lmrMinDepth) {
             int16_t reduction = REDUCTIONS[!capture][depth / 100][moveCount];
 
-            if (stack->ttPv && !pvNode && !cutNode && capture) {
+            if (pvNode && capture) {
+                reduction = movegen.stage == STAGE_PLAY_BAD_CAPTURES ? 1 : 0;
+            } else if (stack->ttPv && !pvNode && !cutNode && capture) {
                 // Do very slight LMR for captures in ttPv-allnodes
                 reduction /= 2;
             } else {
