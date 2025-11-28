be779037e005c6c48ef341f7d282a1679bf744e9
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-11-28 13:50:59 +0100

    Bench: 1721334

--- src/search.cpp
+++ src/search.cpp
@@ -943,12 +943,12 @@ Eval Worker::search(Board* board, SearchStack* stack, Depth depth, Eval alpha, E
 
                 // Futility pruning
                 int fpValue = eval + fpBase + fpFactor * lmrDepth / 100 + pvNode * (fpPvNode + fpPvNodeBadCapture * !bestMove);
-                
+
                 int pieceTo = 2 * 64 * board->pieces[move.origin()] + 2 * move.target() + board->stm;
                 if ((stack - 1)->movedPiece != Piece::NONE)
-                    fpValue += (stack - 1)->contHist[pieceTo] / 500;
+                    fpValue += (stack - 1)->contHist[pieceTo] / 1000;
                 if ((stack - 2)->movedPiece != Piece::NONE)
-                    fpValue += (stack - 2)->contHist[pieceTo] / 500;
+                    fpValue += (stack - 2)->contHist[pieceTo] / 1000;
 
                 if (!capture && lmrDepth < fpDepth && fpValue <= alpha) {
                     movegen.skipQuietMoves();
