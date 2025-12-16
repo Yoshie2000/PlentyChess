721e9be5b76a87659403dfc66d936cab59896df0
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-12-16 21:43:50 +0100

    Bench: 1908127

--- src/search.cpp
+++ src/search.cpp
@@ -949,8 +949,9 @@ Eval Worker::search(Board* board, SearchStack* stack, Depth depth, Eval alpha, E
                 }
             }
             // Futility pruning for bad captures
-            else if (!move.isPromotion() && movegen.stage >= MoveGenStage::STAGE_PLAY_BAD_CAPTURES && !pvNode) {
+            else if (!move.isPromotion() && movegen.stage >= MoveGenStage::STAGE_PLAY_BAD_CAPTURES) {
                 int fpValue = eval + fpBase + fpFactor * lmrDepth / 100 + pvNode * (fpPvNode + fpPvNodeBadCapture * !bestMove);
+                fpValue += moveHistory / 500;
                 Piece capturedPiece = move.isEnpassant() ? Piece::PAWN : board->pieces[move.target()];
                 if (lmrDepth < fpDepth && fpValue + PIECE_VALUES[capturedPiece] <= alpha) {
                     break;
