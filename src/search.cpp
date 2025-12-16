7fd33d2b85259add3d7b1d3d36a1f0bc9dda90e5
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-12-16 08:56:22 +0100

    Bench: 2194974

--- src/search.cpp
+++ src/search.cpp
@@ -942,7 +942,7 @@ Eval Worker::search(Board* board, SearchStack* stack, Depth depth, Eval alpha, E
             if (lmrDepth < fpDepth && fpValue <= alpha) {
                 if (!capture)
                     movegen.skipQuietMoves();
-                else if (!move.isPromotion()) {
+                else {
                     Piece capturedPiece = move.isEnpassant() ? Piece::PAWN : board->pieces[move.target()];
                     if (fpValue + PIECE_VALUES[capturedPiece] <= alpha && movegen.stage >= MoveGenStage::STAGE_PLAY_BAD_CAPTURES)
                         break;
