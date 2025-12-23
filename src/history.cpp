f44f65ef68199ca0e3c328305d751e5ffcdf0b6d
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-12-23 07:38:01 +0100

    Bench: 2328035

--- src/history.cpp
+++ src/history.cpp
@@ -202,13 +202,16 @@ void History::updateSingleCaptureHistory(Board* board, Move move, int16_t bonus)
     *captHistScore += scaledBonus;
 }
 
-void History::updateCaptureHistory(Depth depth, Board* board, Move bestMove, int bestMoveSearchCount, SearchedMoveList& searchedCaptures) {
+void History::updateCaptureHistory(Depth depth, Board* board, Move bestMove, int bestMoveSearchCount, SearchedMoveList& searchedCaptures, bool giveMalus) {
     int captHistBonus = std::min(historyBonusCaptureBase + historyBonusCaptureFactor * depth, historyBonusCaptureMax);
     int captHistMalus = std::min(historyMalusCaptureBase + historyMalusCaptureFactor * depth, historyMalusCaptureMax);
 
     if (board->isCapture(bestMove)) {
         updateSingleCaptureHistory(board, bestMove, captHistBonus * bestMoveSearchCount);
     }
+
+    if (!giveMalus)
+        return;
 
     for (auto& [move, moveSearchCount] : searchedCaptures) {
         if (move == bestMove) continue;
