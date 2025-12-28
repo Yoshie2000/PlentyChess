b411c8d83c64f509e647c190cb70917a21a49354
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-12-28 14:57:07 +0100

    Bench: 1958800

--- src/search.cpp
+++ src/search.cpp
@@ -963,7 +963,7 @@ Eval Worker::search(Board* board, SearchStack* stack, Depth depth, Eval alpha, E
             lmrDepth = std::max<Depth>(0, lmrDepth);
 
             // Futility pruning for captures
-            if (!pvNode && capture && !move.isPromotion()) {
+            if (!pvNode && capture && !move.isPromotion() && !board->givesCheck(move)) {
                 Piece capturedPiece = move.isEnpassant() ? Piece::PAWN : board->pieces[move.target()];
                 if (lmrDepth < fpCaptDepth && eval + fpCaptBase + PIECE_VALUES[capturedPiece] + fpCaptFactor * lmrDepth / 100 <= alpha)
                     continue;
