4f93d613b1688e21a539225a690f835e0edb55a3
Yoshie2000 <patrick.leonhardt@gmx.net>
2026-01-13 14:54:30 +0100

    Bench: 2595979

--- src/search.cpp
+++ src/search.cpp
@@ -547,7 +547,7 @@ movesLoopQsearch:
             if (!SEE(board, move, qsSeeMargin))
                 break;
 
-            if (!move.isPromotion() && moveCount > 2)
+            if (!pvNode && !move.isPromotion() && moveCount > 2)
                 continue;
         }
 
