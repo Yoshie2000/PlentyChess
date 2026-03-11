b090a35efce4c8cb1129454046e64e7874e6feb0
Yoshie2000 <patrick.leonhardt@gmx.net>
2026-03-11 14:47:05 +0100

    Bench: 3864751

--- src/search.cpp
+++ src/search.cpp
@@ -1438,7 +1438,7 @@ void Worker::iterativeDeepening() {
             Eval beta = EVAL_INFINITE;
             Eval value;
 
-            if (depth > 1) {
+            if (rootMoves[0].meanScore != EVAL_NONE) {
                 int updatedOptimism = 150 * rootMoves[0].meanScore / (std::abs(rootMoves[0].meanScore) + 125);
                 optimism[board->stm] = updatedOptimism;
                 optimism[flip(board->stm)] = -updatedOptimism;
