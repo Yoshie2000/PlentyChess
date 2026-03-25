07ebac047fa0168510dd7e737ee9a9c7f7b3d37e
Patrick Leonhardt <40612283+Yoshie2000@users.noreply.github.com>
2026-03-25 17:39:06 +0100

    Bench: 1

--- src/search.cpp
+++ src/search.cpp
@@ -1018,7 +1018,7 @@ Eval Worker::search(Board* board, SearchStack* stack, Depth depth, Eval alpha, E
             && std::abs(ttValue) < EVAL_TBWIN_IN_MAX_PLY
             && ttDepth >= depth - extensionTtDepthOffset
             ) {
-            Eval singularBetaMargin = (ttFlag & TT_EXACTBOUND ? 1 : 2) * depth / 2 + (stack->ttPv && !pvNode) * depth;
+            Eval singularBetaMargin = (ttFlag == TT_EXACTBOUND ? 1 : 2) * depth / 2 + (stack->ttPv && !pvNode) * depth;
             Eval singularBeta = ttValue - singularBetaMargin / 100;
             int singularDepth = (depth - 100) / 2;
 
