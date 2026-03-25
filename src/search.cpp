518c5b2e929576ab1ae25e63ac70fc5347f284f4
Yoshie2000 <patrick.leonhardt@gmx.net>
2026-03-25 16:42:28 +0100

    Bench: 3411297

--- src/search.cpp
+++ src/search.cpp
@@ -959,7 +959,7 @@ Eval Worker::search(Board* board, SearchStack* stack, Depth depth, Eval alpha, E
             Depth lmrDepth = depth - reduction;
 
             // Movecount pruning (LMP)
-            if (!pvNode && moveCount >= LMP_MARGIN[depth / 100][improving]) {
+            if (!pvNode && moveCount >= LMP_MARGIN[depth / 100][improving] + board->givesCheck(move)) {
                 movegen.skipQuietMoves();
             }
 
