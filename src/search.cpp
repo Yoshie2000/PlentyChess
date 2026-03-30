fe84993d73d146d5da906b21127dcb431e5af78e
Yoshie2000 <patrick.leonhardt@gmx.net>
2026-03-30 22:43:52 +0200

    Bench: 4055198

--- src/search.cpp
+++ src/search.cpp
@@ -961,13 +961,7 @@ Eval Worker::search(Board* board, SearchStack* stack, Depth depth, Eval alpha, E
 
             // Futility pruning for quiets
             if (!capture) {
-                int fpValue = eval + fpBase + fpFactor * lmrDepth / 100 + pvNode * (fpPvNode + fpNoBestMove * !bestMove);
-                
-                int chIdx = 2 * 64 * board->pieces[move.origin()] + 2 * move.target() + board->stm;
-                if ((stack - 1)->movedPiece != Piece::NONE)
-                    fpValue += (stack - 1)->contHist[chIdx] / fpConthistDivisor;
-                if ((stack - 2)->movedPiece != Piece::NONE)
-                    fpValue += (stack - 2)->contHist[chIdx] / fpConthistDivisor;
+                int fpValue = eval + fpBase + fpFactor * lmrDepth / 100 + pvNode * (fpPvNode + fpNoBestMove * !bestMove) + moveHistory / fpConthistDivisor;
 
                 if (lmrDepth < fpDepth && fpValue <= alpha) {
                     movegen.skipQuietMoves();
