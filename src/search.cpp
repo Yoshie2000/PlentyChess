add0371941686ff060ea15a887d91c5936559c3d
Yoshie2000 <patrick.leonhardt@gmx.net>
2026-04-15 12:17:47 +0200

    Bench: 3353131

--- src/search.cpp
+++ src/search.cpp
@@ -206,6 +206,10 @@ void initReductions() {
     }
 }
 
+float sigmoid(float x) {
+    return 1.0f / (1.0f + std::exp(-x));
+}
+
 uint64_t perftInternal(Board& board, Depth depth) {
     if (depth == 0) return 1;
 
@@ -1048,8 +1052,12 @@ Eval Worker::search(Board* board, SearchStack* stack, Depth depth, Eval alpha, E
                 ttEntry->update(fmrHash, ttMove, singularDepth, unadjustedEval, value, board->rule50_ply, stack->ttPv, TT_LOWERBOUND);
 
                 // Adjust correction history
-                if (!board->checkers && singularValue > stack->staticEval) {
-                    int bonus = std::clamp((int(singularValue - stack->staticEval) * singularDepth / 100) * correctionHistoryFactorMulticut / 1024, -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
+                if (!board->checkers && singularValue > stack->staticEval && std::abs(singularValue) < EVAL_TBWIN_IN_MAX_PLY) {
+                    float searchWDL = sigmoid(float(singularValue) / NETWORK_SCALE);
+                    float staticWDL = sigmoid(float(stack->staticEval) / NETWORK_SCALE);
+                    int error = (searchWDL - staticWDL) * 2000;
+
+                    int bonus = std::clamp((error * singularDepth / 100) * correctionHistoryFactorMulticut / 1024, -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
                     history.updateCorrectionHistory(board, stack, bonus);
                 }
 
@@ -1265,8 +1273,12 @@ Eval Worker::search(Board* board, SearchStack* stack, Depth depth, Eval alpha, E
         ttEntry->update(fmrHash, bestMove, depth, unadjustedEval, valueToTT(bestValue, stack->ply), board->rule50_ply, stack->ttPv, flags);
 
     // Adjust correction history
-    if (!board->checkers && (!bestMove || !board->isCapture(bestMove)) && (!failHigh || bestValue > stack->staticEval) && (!failLow || bestValue <= stack->staticEval)) {
-        int bonus = std::clamp((int(bestValue - stack->staticEval) * depth / 100) * correctionHistoryFactor / 1024, -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
+    if (!board->checkers && (!bestMove || !board->isCapture(bestMove)) && (!failHigh || bestValue > stack->staticEval) && (!failLow || bestValue <= stack->staticEval) && std::abs(bestValue) < EVAL_TBWIN_IN_MAX_PLY) {
+        float searchWDL = sigmoid(float(bestValue) / NETWORK_SCALE);
+        float staticWDL = sigmoid(float(stack->staticEval) / NETWORK_SCALE);
+        int error = (searchWDL - staticWDL) * 2000;
+
+        int bonus = std::clamp((error * depth / 100) * correctionHistoryFactor / 1024, -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
         history.updateCorrectionHistory(board, stack, bonus);
     }
 
